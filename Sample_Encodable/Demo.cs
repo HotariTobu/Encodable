4259a85d38365e2dae6c55563e9bf330e0d60ab8
hotari24tools <hotari24tools@gmail.com>
2022-03-09 13:46:47 +0900

    add demo

---
+++ Sample_Encodable/Demo.cs
@@ -0,0 +1,30 @@
+﻿using Encodable;
+
+namespace Sample_Encodable
+{
+    public class Demo
+    {
+        class Model : ICodable
+        {
+            private int _Count;
+            public int Count { get => _Count; set => _Count = value; }
+        }
+
+        public Demo()
+        {
+            Demonstration().Wait();
+        }
+
+        private static async Task Demonstration()
+        {
+            Uri uri = new Uri(Path.GetFullPath("file.binary"));
+
+            var savedModel = new Model();
+            savedModel.Count = 5;
+
+            await savedModel.SaveAsync(uri);
+
+            var loadedModel = await uri.LoadAsync<Model>();
+        }
+    }
+}
